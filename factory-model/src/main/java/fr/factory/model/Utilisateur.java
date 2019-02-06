d7a90a826496c6f9469538dd30c9af2b85247795
abonneterre <adrien.bonneterre93@gmail.com>
2019-02-06 16:27:53 +0100

    add acitivite attribute

--- factory-model/src/main/java/fr/factory/model/Utilisateur.java
+++ factory-model/src/main/java/fr/factory/model/Utilisateur.java
@@ -1,11 +1,16 @@
 package fr.factory.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
@@ -52,6 +57,12 @@ public class Utilisateur {
 
 	@OneToOne (mappedBy = "utilisateur", cascade = CascadeType.PERSIST)
 	private Lieu lieu;
+	
+	
+	@OneToMany(mappedBy="lieu")
+	private List<Activite> activites = new ArrayList<Activite>();
+	
+	
 	
 	public int getId() {
 		return id;
