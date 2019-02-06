f0f46bea7e1353615374662dce70b679113b1577
dydy <dylanliner@gmail.com>
2019-02-06 16:41:11 +0100

    template crud categorie

--- factory-springboot/src/main/java/fr/factory/dao/IDAOCategorie.java
+++ factory-springboot/src/main/java/fr/factory/dao/IDAOCategorie.java
@@ -2,6 +2,8 @@ package fr.factory.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import fr.factory.model.Categorie;
+
 public interface IDAOCategorie extends JpaRepository<Categorie, Integer>{
 	
 	
