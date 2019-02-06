07876dff20173ca0440b4054d946064ec3ca9bd9
Guenael <guenael.sacheau@soprasteria.com>
2019-02-06 15:29:33 +0100

    COnfig des routes -angular

--- factory-angular/src/app/app.module.ts
+++ factory-angular/src/app/app.module.ts
@@ -1,8 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { AccueilComponent } from './accueil/accueil.component';
+
+// Config des routes :
+const routes: Routes = [
+{ path: 'accueil', component: AccueilComponent },
+{ path: '', redirectTo: 'home', pathMatch: 'full' },
+];
 
 @NgModule({
   declarations: [
@@ -10,7 +17,8 @@ import { AccueilComponent } from './accueil/accueil.component';
     AccueilComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
