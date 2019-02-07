c447677e4c0b9ed7d08a2ed4ae796fafa4219ea2
Guenael <guenael.sacheau@soprasteria.com>
2019-02-07 14:17:07 +0100

    Categories dans navbar

--- factory-angular/src/app/accueil/accueil.component.ts
+++ factory-angular/src/app/accueil/accueil.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { CategorieService } from '../categorie.service';
+
+
+
 
 @Component({
   selector: 'app-accueil',
   templateUrl: './accueil.component.html',
-  styleUrls: ['./accueil.component.css']
+  styleUrls: ['./accueil.component.css'],
+  providers: [CategorieService]
 })
 export class AccueilComponent implements OnInit {
 
-  constructor() { }
+  constructor(public categorieService: CategorieService) { }
 
   ngOnInit() {
   }
