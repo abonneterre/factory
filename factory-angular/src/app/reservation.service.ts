e0b7a665c0623c1e20a87144d88a8f57c65e00df
Benjamin <benjibee9217@gmail.com>
2019-02-07 14:55:50 +0100

    creation service reservation

--- factory-angular/src/app/reservation.service.ts
+++ factory-angular/src/app/reservation.service.ts
@@ -1,10 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReservationService {
-  
 
-  constructor() { }
+
+  constructor(private httpClient: HttpClient) { }
+
+  save(reservation:Reservation){
+    this.httpClient
+        .post("http://localhost:8080/api/reservation",reservation)
+        .subscribe();
+      }
+
+
 }
--- factory-angular/src/app/reservation/reservation.component.ts
+++ factory-angular/src/app/reservation/reservation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ReservationService } from '../categorie.service';
 
 @Component({
   selector: 'app-reservation',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReservationComponent implements OnInit {
 
-  constructor() { }
+  constructor(public reservationService: ReservationService) { }
 
   ngOnInit() {
   }
