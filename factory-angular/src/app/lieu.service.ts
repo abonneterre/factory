f2c71a45932184458f5c2815d25b8eb06da1ca12
Guenael <guenael.sacheau@soprasteria.com>
2019-02-07 15:03:45 +0100

    LieuService

---
+++ factory-angular/src/app/lieu.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LieuService {
+    public lieu: any = null;
+
+  constructor(private httpClient: HttpClient) { }
+
+  findAllAsync() {
+     if (this.lieu == null) {
+  this.lieu =  this.httpClient.get("http://localhost:8080/api/lieu");
+  }
+
+   return this.lieu;
+
+
+  }
+
+      refresh() {
+        this.lieu = null;
+      }
+
+}
