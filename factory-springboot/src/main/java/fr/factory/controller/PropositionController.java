9151f1d45a3aa3f6b9510549917d9dc1a351950e
Guenael <guenael.sacheau@soprasteria.com>
2019-02-08 11:01:00 +0100

    Propositions Vue

--- factory-springboot/src/main/java/fr/factory/controller/PropositionController.java
+++ factory-springboot/src/main/java/fr/factory/controller/PropositionController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import fr.factory.dao.IDAOProposition;
 import fr.factory.model.Proposition;
@@ -21,5 +22,14 @@ public class PropositionController {
 	List <Proposition> mesPropositions = daoProposition.findAll();
 	model.addAttribute("mesPropositions", mesPropositions); 
 	return "propositions";
+	}
+	
+	@GetMapping("/archiver/{id}")
+	public String archiver(@PathVariable int id) {
+		Proposition proposition= daoProposition.findById(id).get();
+		proposition.setArchivee(true);
+		daoProposition.save(proposition);
+		return "redirect:/propositions";
+		
 	}
 }
