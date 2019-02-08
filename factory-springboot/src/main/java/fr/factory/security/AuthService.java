a693b66f5c6924ffe2677c7dc490f259e3c901cd
dydy <dylanliner@gmail.com>
2019-02-08 10:25:00 +0100

    security

--- factory-springboot/src/main/java/fr/factory/security/AuthService.java
+++ factory-springboot/src/main/java/fr/factory/security/AuthService.java
@@ -1,5 +1,24 @@
 package fr.factory.security;
 
-public class AuthService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import fr.factory.dao.IDAOUtilisateur;
+
+
+@Service
+public class AuthService implements UserDetailsService {
+	
+	@Autowired
+	IDAOUtilisateur daoUtilisateur;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		// TODO Auto-generated method stub
+		return new UtilisateurPrincipal(daoUtilisateur.findByUsername(username));
+	}
 
 }
